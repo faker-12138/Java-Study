39816e21c5201c7adbf2aaf8095d7f413ec0b32a
faker-12138 <2936881902@qq.com>
2024-07-30 14:23:56 +0800

    1

---
+++ 01-数组/src/Test.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+/**
+ * @author 张涛
+ * @date 2024-07-30 14:20
+ */
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+    }
+}
